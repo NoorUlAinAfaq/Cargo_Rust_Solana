6aec0f80a0c45ded26f10c36ba6af5ea8c93d4f6
Noor Ul Ain Afaq <noorulain.afaqkhatri@gmail.com>
2024-09-09 19:46:46 +0500

    structs

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,17 +1,40 @@
 fn main() {
-   let arr = [1 , 2, 3, 4];
-   let slice = &arr[1..4];
-   println!("{:?}", slice);
-   println!("{}", slice.len());
+   // let arr = [1 , 2, 3, 4];
+   // let slice = &arr[1..4];
+   // println!("{:?}", slice);
+   // println!("{}", slice.len());
 
-   let str = " Hello Cargo"; //creates a variable
-   println!("{}", str);
+   // let str = " Hello Cargo"; //creates a variable
+   // println!("{}", str);
 
-   let string = String::from(str); // creates an object
-   println!("{}", string.);
+   // let string = String::from(str); // creates an object
+   // println!("{}", string.);
 
-   let slice = &string[6..];
-   println!("{} {}", slice, string.len());
+   // let slice = &string[6..];
+   // println!("{} {}", slice, string.len());
+   let name = String::from("Ali");
+   //let number = 6;
+   let student = Student{name, rollnumber : 4};
+   student.print_name();
+   struct Student
+   {
+      name : String,
+      rollnumber : u64
+   }
+
+   impl Student {
+      fn print_name(&self)
+      {
+         println!("{} {}", self.name, self.rollnumber);
+      }
+       
+   }
 
 
+}
+
+struct Student
+{
+   name : String,
+   
 }
