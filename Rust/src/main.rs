e293579be28870cab5b3bc01e132ab2ebe551989
Noor Ul Ain Afaq <noorulain.afaqkhatri@gmail.com>
2024-09-09 20:09:30 +0500

    Traits

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+// fn main() {
    // let arr = [1 , 2, 3, 4];
    // let slice = &arr[1..4];
    // println!("{:?}", slice);
@@ -12,29 +12,58 @@ fn main() {
 
    // let slice = &string[6..];
    // println!("{} {}", slice, string.len());
-   let name = String::from("Ali");
-   //let number = 6;
-   let student = Student{name, rollnumber : 4};
-   student.print_name();
-   struct Student
-   {
-      name : String,
-      rollnumber : u64
-   }
+   // let name = String::from("Ali");
+   // //let number = 6;
+   // let student = Student{name, rollnumber : 4};
+   // student.print_name();
+   // struct Student
+   // {
+   //    name : String,
+   //    rollnumber : u64
+   // }
 
-   impl Student {
-      fn print_name(&self)
-      {
-         println!("{} {}", self.name, self.rollnumber);
-      }
+   // impl Student {
+   //    fn print_name(&self)
+   //    {
+   //       println!("{} {}", self.name, self.rollnumber);
+   //    }
        
+   // }
+
+
+
+//}
+
+
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
+
+pub trait Summary {
+   fn summarize(&self) -> String;
+}
+
+
+pub struct Tweet {
+   pub username: String,
+   pub content: String,
+   pub reply: bool,
+   pub retweet: bool,
+}
+
+impl Summary for Tweet {
+   fn summarize(&self) -> String {
+       format!("{}: {}", self.username, self.content)
    }
-
-
 }
 
-struct Student
-{
-   name : String,
-   
-}
